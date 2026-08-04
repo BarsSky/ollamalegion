27cf3befcd1352d91c8f7bc8e0dda13f2b08f79c
BarsSky <knagaenko@mail.ru>
2026-08-04 20:54:45 +0300

    chore: drop commit message file

---
+++ logs/build_v0513_20260804_191850.ps1
@@ -0,0 +1,12 @@
+﻿Set-Location C:\Ollama\ollamalegion
+$env:CUDA_ARCH = '86'
+$log = 'C:\Ollama\ollamalegion\logs\build_v0513_20260804_191850.log'
+function Log($msg) { Add-Content -Path $log -Value $msg -Encoding utf8 }
+Log "=== Build started at $(Get-Date) ==="
+try {
+    & .\scripts\build-containers.ps1 -CppWorker -CudaArch '86' *>&1 | ForEach-Object { Log "$_" }
+} catch {
+    Log "ERROR: $($_.Exception.Message)"
+    Log $_.ScriptStackTrace
+}
+Log "=== Build finished at $(Get-Date) ==="
---
+++ logs/build_webui_v0513_20260804_192927.ps1
@@ -0,0 +1,12 @@
+﻿Set-Location C:\Ollama\ollamalegion
+$env:DOCKER_BUILDKIT = '1'
+$log = 'C:\Ollama\ollamalegion\logs\build_webui_v0513_20260804_192927.log'
+function Log($msg) { Add-Content -Path $log -Value $msg -Encoding utf8 }
+Log "=== WebUI build started at $(Get-Date) ==="
+try {
+    & docker compose -f deployments/docker-compose.yml build webui 2>&1 | ForEach-Object { Log "$_" }
+} catch {
+    Log "ERROR: $($_.Exception.Message)"
+    Log $_PSItem.ScriptStackTrace
+}
+Log "=== WebUI build finished at $(Get-Date) ==="
